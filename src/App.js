6ba1fb8fed20206ce6abe0a86163c04f357f310c
Uyoyo Whiskey <uyoyowhiskey21@gmail.com>
2023-08-04 14:30:26 +0100

    Implement Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage/LandingPage';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Happyhub</h1>
-    </div>
+    <Routes>
+      <Route exact path='/' element={<LandingPage />} />
+    </Routes>
   );
 }
 
