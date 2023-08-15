75663bb5f12fd5eb68ed3edcd8b15d5c070bd389
Uyoyo Whiskey <uyoyowhiskey21@gmail.com>
2023-08-15 15:28:51 +0100

    Refactor code for Info screen

--- src/components/login-pages/SignUp.jsx
+++ src/components/login-pages/SignUp.jsx
@@ -11,11 +11,11 @@ function SignUp() {
           <img src={logo} className="app-logo" alt="logo" />
           <h1 className='signup-header'>Sign Up</h1>
           <form className='sign-up-form'>
-              <input placeholder='Email' type='email' name='email' className='mail'></input>
-              <input placeholder='Username' type='text' name='name' className='username'></input>
-              <input placeholder='Create Password' type='password' name="password" className='pass'></input>
-              <input placeholder='Confirm Password' type='password' name="password" className='passconfirm'></input>
-              <button className='continue-btn'><Link to='/Info' className='decor'>Continue</Link></button>
+            <input placeholder='Email' type='email' name='email' className='mail'></input>
+            <input placeholder='Username' type='text' name='name' className='username'></input>
+            <input placeholder='Create Password' type='password' name="password" className='pass'></input>
+            <input placeholder='Confirm Password' type='password' name="password" className='passconfirm'></input>
+            <button className='continue-btn'><Link to='/info' className='decor'>Continue</Link></button>
           </form>
         </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
 import SignIn from './components/login-pages/Signin';
 import SignUp from './components/login-pages/Signup';
-// import Info from './components/login-pages/Info';
+import Info from './components/login-pages/Info';
 // import Contact from './components/login-pages/Contact';
 // import AiChat from './components/chat-pages/Aichat';
 // import Settings from './components/chat-pages/Settings';
@@ -19,8 +19,8 @@ function App() {
         <Route path="/" element={< LandingPage />} />
         <Route path="sign-in" element={< SignIn />} />
         <Route path="sign-up" element={< SignUp />} />
-        {/* <Route path="info" element={< Info />} />
-        <Route path="contact" element={< Contact />} />
+        <Route path="info" element={< Info />} />
+        {/* <Route path="contact" element={< Contact />} />
         <Route path="aiChat" element={< AiChat />} />
         <Route path="settings" element={< Settings />}  />
         <Route path="get-help" element={< GetHelp />}  /> */}
